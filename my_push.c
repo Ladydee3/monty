2f196eae0b259d21d8a5d62f5bf5697e7abb750e
Ladydee3 <delisiletwala62@gmail>
2023-12-14 14:26:13 -0800

    MY Stacks, Queues - LIFO, FIFO

--- my_push.c
+++ my_push.c
@@ -32,7 +32,7 @@ void f_my_push(stack_t **head, unsigned int counter)
 	exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
 	if (bus. lifi == 0)
-	addnode(head, n);
+	addnode (head, n);
 	else
 	addqueue(head, n);
 }
